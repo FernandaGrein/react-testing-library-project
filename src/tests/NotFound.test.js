656c7788c9fe57a2dba6590682828ee2a7d5f6ae
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-06-06 16:59:30 -0300

    adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente NotFound', () => {
+  it('se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const titleText = screen.getByRole('heading',
+      { name: /Page requested not found Crying emoji/i });
+
+    expect(titleText).toBeInTheDocument();
+  });
+
+  it('se a página mostra a imagem ', () => {
+    renderWithRouter(<NotFound />);
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const ImgElement = screen.getByRole('img', { name: /Pikachu crying/i });
+
+    expect(ImgElement).toHaveAttribute('src', imgSrc);
+  });
+});
