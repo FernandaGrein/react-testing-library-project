a0a06c58318655424e9cba4355a4974202c9e0f0
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-06-06 16:40:22 -0300

    adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Requisito 3 Teste o componente FavoritePokemons', () => {
+  it(`se é exibida na tela a mensagem No favorite pokemon found, 
+  caso a pessoa não tenha pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteText = screen.getByText(/No favorite pokemon found/i);
+
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+
+//   it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+//     renderWithRouter(<FavoritePokemons />);
+//     renderWithRouter(<PokemonDetails />);
+//     import PokemonDetails
+//   });
+});
